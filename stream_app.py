316c5ef623eb9744dfa0f4316ebc7aeca16e1706
md202307 <139216118+md202307@users.noreply.github.com>
2023-07-13 08:52:43 +0200

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -1,3 +1,6 @@
+stremlit.stop()
+import stremlit
 import pandas
 import snowflake.connector
+from urllib.error import URLError
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
